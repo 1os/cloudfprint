189da2244621deeb31bb646a712ec6f8869ba86b
lazareveugene <157624586>
2019-08-27 15:25:42 +0500

    ...

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,3 +1,7 @@
 sudo snap remove cloudfprint
 snapcraft cleanbuild
 sudo snap install --devmode --edge cloudfprint_0.1_amd64.snap
+
+sudo snap connect cloudfprint:io-ports-control :io-ports-control
+sudo snap connect cloudfprint:hardware-observe :hardware-observe
+sudo snap connect cloudfprint:raw-usb :raw-usb

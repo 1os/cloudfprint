6fa9d158d91ee39dd59e77071dfc7c6dd4639f98
lazareveugene <157624586>
2019-12-20 13:50:22 +0500

    ...

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,6 +1,6 @@
 sudo snap remove cloudfprint
 snapcraft cleanbuild
-sudo snap install --devmode --edge cloudfprint_0.1_amd64.snap
+sudo snap install --devmode --edge cloudfprint_2019.12.06_multi.snap
 
 sudo snap connect cloudfprint:io-ports-control :io-ports-control
 sudo snap connect cloudfprint:hardware-observe :hardware-observe

b0d8e77784c1a45bfdeb02b9b82ff1e0d872b1e7
Lazarev Eugene <lazareveugene@gmail.com>
2020-08-25 14:47:49 +0500

    ,,,

--- assets/run.sh
+++ assets/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd $SNAP/
-$SNAP/usr/lib/jvm/default-java/bin/java -Duser.language=RU -Duser.region=ru -Djava.library.path=libs/ -jar cloudfprint.jar start
+${SNAP}/usr/lib/jvm/java-8-openjdk-${SNAP_ARCH}/bin/java -Duser.language=RU -Duser.region=ru -Djava.library.path=libs/ -jar cloudfprint.jar start

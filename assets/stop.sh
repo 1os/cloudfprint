813d8a8a6127d6e89868945110e0f657c10ab347
Lazarev Eugene <lazareveugene@gmail.com>
2020-10-14 14:46:42 +0500

    2020.10.14

--- assets/stop.sh
+++ assets/stop.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-$SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH/bin/java -jar $SNAP/cloudfprint.jar stop
+cd $SNAP/
+$SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH/bin/java -Duser.language=RU -Duser.region=ru -Djava.library.path=libs/ -jar cloudfprint.jar stop

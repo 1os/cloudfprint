88b5d61b41f12bd377e07c2bb43e4c5027107191
Lazarev Eugene <lazareveugene@gmail.com>
2021-03-09 11:56:22 +0500

    ...

--- assets/run.sh
+++ assets/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sleep 10s
+sleep 20s
 cd $SNAP/
 $SNAP/usr/lib/jvm/java-11-openjdk-$SNAP_ARCH/bin/java -Duser.language=RU -Duser.region=ru -Djava.library.path=libs/ -jar cloudfprint.jar start
